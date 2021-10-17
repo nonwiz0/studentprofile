7941e86389ea3363ed874ecad1e0d4d978604722
Chan Rose <chanrose@pm.me>
2021-10-17 12:34:56 +0700

    removed testing

--- studentprofile/settings.py
+++ studentprofile/settings.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = [
     'tailwind',
     'theme',
     'spapp',
-    'testhtmx',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- testhtmx/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "htmx"
-urlpatterns = [
-    path('create-book/<pk>/', views.create_book, name="create-book"),
-    path('', views.my_view, name="home"),
-]
